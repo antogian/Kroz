e97f86d94510945fb15276d778ced889956f87aa
Eleni Aidonidou <elenaidon@gmail.com>
2015-03-21 19:25:46 +0200

    Removed SceneId Attribute. changes <Scene.java>

--- src/com/kroz/scenes/Scene.java
+++ src/com/kroz/scenes/Scene.java
@@ -5,11 +5,7 @@
  */
 package com.kroz.scenes;
 
-import com.kroz.player.Player;
-import com.kroz.scenario.IScenario;
-
 public class Scene {
-    private int sceneId;
     private String sceneDescription;
 
     public Scene() {
@@ -20,41 +16,18 @@ public class Scene {
      * Initializes the attributes of a new Scene object.
      */
     private void initialize() {
-        sceneId = -1;
         sceneDescription = "Default Scene Description.";
     }
 
     /**
      * Constructor with arguments for Scene objects.
-     * @param sceneId new Scene object's ID.
      * @param sceneDescription new Scene object's description.
      */
-    public Scene(int sceneId, String sceneDescription) {
-        this.sceneId = sceneId;
+    public Scene(String sceneDescription) {
         this.sceneDescription = sceneDescription;
-    }
-
-    public int getSceneId() {
-        return sceneId;
     }
 
     public String getSceneDescription() {
         return sceneDescription;
-    }
-    
-    /**
-     * Extracts the scene in which Player is acting (i.e. where he is currently in).
-     * @param player used to define where player is through playerCurrentSceneId attribute of Player class.
-     * @param scenario used to define scenario's scene list through defaultScenarioSceneList attribute of DefaultScenario class.
-     * @return the scene where the player currently is in.
-     */
-    public Scene extractCurrentScene(Player player, IScenario scenario) {
-        Scene tempScene = new Scene();
-        for (Scene aScene : scenario.getScenarioSceneList()) {
-            if (aScene.getSceneId() == player.getPlayerCurrentSceneId()) {
-                tempScene = aScene;
-            }
-        }
-        return tempScene;
     }
 }
