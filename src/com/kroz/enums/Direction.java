f6ca2974c17aded343558b4a9656b84078927f51
Tony <Tony@Tony-PC.lan>
2015-03-23 21:16:30 +0200

    Enum Direction

---
+++ src/com/kroz/enums/Direction.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kroz.enums;
+
+/**
+ *
+ * @author Tony
+ */
+public enum Direction {
+    N("NORTH"),
+    NW("NORTHWEST"), NE("NORTHEAST"),
+    W("WEST"), E("EAST"),
+    SW("SOUTHWEST"), SE("SOUTHEAST"),
+    S("SOUTH");
+    
+    private String name;
+    
+    Direction(String name){
+        this.name = name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
