1d5e017892a31997a5ce3af43efc4b338532a5f8
Eleni Aidonidou <elenaidon@gmail.com>
2015-03-23 21:40:28 +0200

    added main back

---
+++ src/com/kroz/Main.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kroz;
+
+import com.kroz.commands.ICommand;
+import com.kroz.parsers.CommandParser;
+import com.kroz.parsers.ConsoleInputParser;
+import com.kroz.player.Player;
+import com.kroz.scenario.DefaultScenario;
+import com.kroz.scenario.IScenario;
+import com.kroz.scenes.Scene;
+
+
+/**
+ * Main Class.
+ * @author Eleni Aidonidou
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        IScenario scenario = new DefaultScenario();
+        Player player = new Player("The Player!");
+        ConsoleInputParser consoleInputParser = new ConsoleInputParser();
+        CommandParser commandParser = new CommandParser();
+        ICommand currentCommand;
+
+        //Gets new Player Name and prints out The new players name
+        player.setPlayerName();
+        player.getPlayerName();
+        //Sets the players current scene to the first scene of the scenario
+        player.setPlayerCurrentScene(scenario.getScenarioSceneList().get(0));
+
+         //Prints out the Scenes of the current Scenario
+        System.out.println("Scene List:");
+        for (Scene scene : scenario.getScenarioSceneList()) {
+            System.out.println(new StringBuilder()
+                    .append(" Scene Description: ").append(scene.getSceneDescription())
+                    .append("\n").toString());
+        }
+
+        System.out.println(player.getPlayerCurrentScene().getSceneDescription());
+
+        while (!scenario.isScenarioComplete()) {
+            commandParser.setRawCommandText(consoleInputParser.getInputFromConsole());
+            try {
+                currentCommand = commandParser.createCommand(player, scenario);
+                currentCommand.executeCommand();
+            } catch (IllegalArgumentException e) {
+            /*if exception is caused user is prompted to enter
+            another command so there is no need for catch code*/
+            }
+        }
+
+        //should be at the end of the main method
+        consoleInputParser.closeStream();
+    }
+
+}
