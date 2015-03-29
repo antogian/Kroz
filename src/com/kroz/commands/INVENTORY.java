ff3a5d1b337ab9a8a59e3d64126a6df68237de83
Eleni Aidonidou <elenaidon@gmail.com>
2015-03-29 18:42:16 +0300

    Added Command INVENTORY. that shows the innventory of the current player

---
+++ src/com/kroz/commands/INVENTORY.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kroz.commands;
+
+import com.kroz.player.Player;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+/**
+ *
+ * @author Eleni
+ */
+public class INVENTORY implements ICommand{
+    private Player currentPlayer;
+    private List<String> currentCommandTextList;
+
+    public INVENTORY() {
+        initialize();
+    }
+    private void initialize() {
+        currentPlayer = new Player();
+        currentCommandTextList = new ArrayList<String>();
+    }
+    @Override
+    public void executeCommand() {
+        isValid();
+        try {
+            this.currentPlayer.getPlayerInventory().showInventory();
+        } catch (EmptyStackException e) {
+            System.out.println("You have no items.");
+        }
+
+    }
+
+    @Override
+    public void setCurrentPlayer(Player newCurrentPlayer) {
+        this.currentPlayer = newCurrentPlayer;
+    }
+
+    @Override
+    public void setCommandTextList(List<String> newRawCommandText) {
+        this.currentCommandTextList = newRawCommandText;
+    }
+
+    @Override
+    public void isValid() {
+        if (!this.currentCommandTextList.isEmpty()) {
+            System.out.println("Command INVENTORY doesn't get any parameters. Try: [INVENTORY]");
+            throw new IllegalArgumentException();
+        }
+    }
+    
+}
