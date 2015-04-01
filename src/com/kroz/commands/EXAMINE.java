bd5c5f969c7778932ccdf39e5938a3d28b125f58
Tony <Tony@Tony-PC.lan>
2015-04-01 20:05:01 +0300

    Added <EXAMINE.java> and <USE.java> of commands package

---
+++ src/com/kroz/commands/EXAMINE.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kroz.commands;
+
+import com.kroz.items.Item;
+import com.kroz.player.Player;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tony
+ */
+public class EXAMINE implements ICommand{
+
+    private Player currentPlayer;
+    private List<String> currentCommandTextList;
+    private Item currentItem;
+    
+    
+    public EXAMINE(){
+        this.initialize();
+    }
+    
+    public void initialize(){
+        this.currentPlayer = new Player();
+        this.currentCommandTextList = new ArrayList<>();
+    }
+    
+    @Override
+    public void executeCommand(){
+        this.isValid();
+        if (this.itemExists()) {
+                System.out.println(this.currentItem.getItemDescription());
+        }
+        else {
+            System.out.println(this.currentItem.getItemName() + " doesn't exist");
+        }
+    }
+    
+    @Override
+    public void setCurrentPlayer(Player newCurrentPlayer){
+        this.currentPlayer = newCurrentPlayer;
+    }
+    
+    @Override
+    public void setCommandTextList(List<String> newRawCommandText){
+        this.currentCommandTextList = newRawCommandText;
+    }
+    
+    public void setCurrentItem(Item item){
+        this.currentItem = item;
+    }
+    
+    @Override
+    public void isValid(){
+        if (this.currentCommandTextList.size() != 1) {
+           System.out.println("Command EXAMINE takes one parameter. Try: [EXAMINE parameter]");
+           throw new IllegalArgumentException();
+        }
+    }
+    
+    public boolean itemExists(){
+        for(Item tempItem : this.currentPlayer.getPlayerCurrentScene().getSceneInventory().getItemList()){
+            if (tempItem.getItemName().equalsIgnoreCase(this.currentCommandTextList.get(0))){
+                setCurrentItem(tempItem);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
---
+++ src/com/kroz/commands/USE.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kroz.commands;
+
+import com.kroz.items.Item;
+import com.kroz.player.Player;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tony
+ */
+public class USE implements ICommand{
+    
+    private Player currentPlayer;
+    private List<String> currentCommandTextList;
+    private Item currentItem;
+    
+    
+    public USE(){
+        this.initialize();
+    }
+    
+    public void initialize(){
+        this.currentPlayer = new Player();
+        this.currentCommandTextList = new ArrayList<>();
+    }
+    
+    @Override
+    public void executeCommand(){
+        this.isValid();
+        if (this.itemExists()) {
+                this.currentItem.useItem();
+        }
+        else {
+            System.out.println(this.currentItem.getItemName() + " doesn't exist");
+        }
+    }
+    
+    @Override
+    public void setCurrentPlayer(Player newCurrentPlayer){
+        this.currentPlayer = newCurrentPlayer;
+    }
+    
+    @Override
+    public void setCommandTextList(List<String> newRawCommandText){
+        this.currentCommandTextList = newRawCommandText;
+    }
+    
+    public void setCurrentItem(Item item){
+        this.currentItem = item;
+    }
+    
+    @Override
+    public void isValid(){
+        if (this.currentCommandTextList.size() != 1) {
+           System.out.println("Command USE takes one parameter. Try: [USE parameter]");
+           throw new IllegalArgumentException();
+        }
+    }
+    
+    public boolean itemExists(){
+        for(Item tempItem : this.currentPlayer.getPlayerInventory().getItemList()){
+            if (tempItem.getItemName().equalsIgnoreCase(this.currentCommandTextList.get(0))){
+                setCurrentItem(tempItem);
+                return true;
+            }
+        }
+        return false;
+    }
+}
