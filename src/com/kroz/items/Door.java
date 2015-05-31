9e8167420e764903f6c4e64658207d9148483267
Tony <Tony@Tony-PC.lan>
2015-05-31 19:50:02 +0300

    Modifications at <Key>, <Door> classes of items package.

--- src/com/kroz/items/Door.java
+++ src/com/kroz/items/Door.java
@@ -1,5 +1,9 @@
 package com.kroz.items;
 
+import com.kroz.commands.CLOSE;
+import com.kroz.commands.LOCK;
+import com.kroz.commands.OPEN;
+import com.kroz.commands.UNLOCK;
 import com.kroz.enums.ItemType;
 import com.kroz.enums.ItemState;
 
@@ -8,10 +12,28 @@ import com.kroz.enums.ItemState;
 * @author Immortuon
 */
 public class Door extends Item {
-
+    
+    private ItemState lock;
+    private Key doorKey;
+    
     public Door() {
         super("Door", "a door", ItemType.SCENE_OBJECT, ItemState.DEFAULT);
+        this.setLegalActions();
     }
+    
+    public Door(ItemState lockState) {
+        super("Door", "a door", ItemType.SCENE_OBJECT, ItemState.DEFAULT);
+        this.lock = lockState;
+        this.setLegalActions();
+    }
+    
+    public void setLegalActions(){
+        OPEN.addOpenableItem(this);
+        CLOSE.addCloseableItem(this);
+        UNLOCK.addUnlockableItem(this);
+        LOCK.addLockableItem(this);
+    }
+    
     public void useItem(){
         throw new UnsupportedOperationException("Not supported yet.");
     }
@@ -26,6 +48,43 @@ public class Door extends Item {
         }
         else if (itemState == ItemState.ENABLED){
             itemState = ItemState.DISABLED;
+        }
+    }
+    
+    public void lockDoor(){
+        if(this.lock == ItemState.DEFAULT){
+            System.out.println("This door has no lock.");
+        }
+        else if (this.lock == ItemState.ENABLED){
+            System.out.println("The door is already locked.");
+        }
+        else {
+            this.lock = ItemState.ENABLED;
+        }
+    }
+    
+    public void unlockDoor(){
+        if(this.lock == ItemState.DEFAULT){
+            System.out.println("This door has no lock.");
+        }
+        else if (this.lock == ItemState.DISABLED){
+            System.out.println("The door is already unlocked.");
+        }
+        else {
+            this.lock = ItemState.DISABLED;
+        }
+    }
+    
+    public void setLockState(ItemState newLockState){
+        this.lock = newLockState;
+    }
+    
+    public boolean isItemLocked(){
+        if (this.lock == ItemState.ENABLED){
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
--- src/com/kroz/items/Key.java
+++ src/com/kroz/items/Key.java
@@ -5,6 +5,8 @@
  */
 package com.kroz.items;
 
+import com.kroz.commands.DROP;
+import com.kroz.commands.TAKE;
 import com.kroz.enums.ItemState;
 import com.kroz.enums.ItemType;
 
@@ -13,14 +15,41 @@ import com.kroz.enums.ItemType;
  * @author Eleni
  */
 public class Key extends Item {
+    
+    private Door currentItem;
+    
     public Key() {
         super("Key", "a key that unlocks a door", ItemType.PLAYER_OBJECT, ItemState.DISABLED);
+        this.currentItem = new Door();
+        this.setLegalActions();
     }
-    public void useItem(){
-        throw new UnsupportedOperationException("Not supported yet.");
+    
+    public Key(Door newDoor){
+        super("Key", "a key that unlocks a door", ItemType.PLAYER_OBJECT, ItemState.DISABLED);
+        this.currentItem = newDoor;
+        this.setLegalActions();
     }
+    
+    public void unlockItem(){
+        this.currentItem.unlockDoor();
+    }
+    
+    public void lockItem(){
+        this.currentItem.lockDoor();
+    }
+    
     @Override
     public void changeItemState() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public void setCurrentItem(Item newItem){
+        this.currentItem = (Door)newItem;
+    }
+    
+    public void setLegalActions(){
+        TAKE.addTakeableItem(this);
+        DROP.addDroppableItem(this);
+    }
+    
 }
