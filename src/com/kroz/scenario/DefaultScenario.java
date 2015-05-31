6590e392c194000e280c3a35319baa185b690f46
Tony <Tony@Tony-PC.lan>
2015-05-31 19:53:38 +0300

    Minor changes at <DefaultScenario>

--- src/com/kroz/scenario/DefaultScenario.java
+++ src/com/kroz/scenario/DefaultScenario.java
@@ -15,6 +15,11 @@ import com.kroz.enums.ItemState;
 import com.kroz.items.Door;
 import com.kroz.items.Key;
 import com.kroz.items.Torch;
+import com.kroz.scene.templates.Basement;
+import com.kroz.scene.templates.Forest;
+import com.kroz.scene.templates.Garden;
+import com.kroz.scene.templates.Kitchen;
+import com.kroz.scene.templates.Room;
 
 
 public class DefaultScenario implements IScenario {
@@ -65,17 +70,17 @@ public class DefaultScenario implements IScenario {
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(2),
                 new SceneExit(Direction.E, this.getScenarioSceneList().get(0)));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(2),
-                new SceneExit(Direction.S, this.getScenarioSceneList().get(3), ItemState.ENABLED));
+                new SceneExit(Direction.S, this.getScenarioSceneList().get(3), ItemState.DISABLED, ItemState.ENABLED));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(3),
-                new SceneExit(Direction.E, this.getScenarioSceneList().get(1), ItemState.DISABLED));
+                new SceneExit(Direction.E, this.getScenarioSceneList().get(1)));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(3),
-                new SceneExit(Direction.S, this.getScenarioSceneList().get(4), ItemState.DISABLED));
+                new SceneExit(Direction.S, this.getScenarioSceneList().get(4)));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(3),
-                new SceneExit(Direction.N, this.getScenarioSceneList().get(2), ItemState.ENABLED));
+                new SceneExit(Direction.N, this.getScenarioSceneList().get(2)));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(1),
                 new SceneExit(Direction.W, this.getScenarioSceneList().get(3)));
         this.defaultScenarioMap.addExitToScene(this.getScenarioSceneList().get(4),
-                new SceneExit(Direction.N, this.getScenarioSceneList().get(3), ItemState.ENABLED));
+                new SceneExit(Direction.N, this.getScenarioSceneList().get(3), ItemState.ENABLED, ItemState.DISABLED));
     }
     /**
      *Setting of the default scenario scenes.
@@ -83,23 +88,23 @@ public class DefaultScenario implements IScenario {
     @Override
     public final void setScenarioSceneList() {
         Scene newScene;
-        newScene = new Scene("You are in a forest. West there is a garden.");
-        newScene.getSceneInventory().addItemInInventory(new Door());
-        newScene.getSceneInventory().addItemInInventory(new Torch());
+//        newScene = new Scene("You are in a forest. West there is a garden.");
+//        newScene.getSceneInventory().addItemInInventory(new Door());
+//        newScene.getSceneInventory().addItemInInventory(new Torch());
+//        newScene.getSceneInventory().addItemInInventory(new Key());
+        this.defaultScenarioSceneList.add(newScene = new Forest());
+//        newScene = new Scene("You are in a kitchen. West there is a door leading to a room.");
         newScene.getSceneInventory().addItemInInventory(new Key());
-        this.defaultScenarioSceneList.add(newScene);
-        newScene = new Scene("You are in a kitchen. West there is a door leading to a room.");
-        newScene.getSceneInventory().addItemInInventory(new Door());
-        this.defaultScenarioSceneList.add(newScene);
-        newScene = new Scene("You are in a garden. East there is a forest."
-                + " South there is a house. The door is open.");
-        this.defaultScenarioSceneList.add(newScene);
-        newScene = new Scene("You are in a room. East there is a kitchen."
-                + " South you can see a staircase leading to the basement."
-                + " North is an open door leading to a garden.");
-        this.defaultScenarioSceneList.add(newScene);
-        newScene = new Scene("You are in a basement. North there is a staircase leading upwards.");
-        this.defaultScenarioSceneList.add(newScene);
+        this.defaultScenarioSceneList.add(newScene = new Kitchen());
+//        newScene = new Scene("You are in a garden. East there is a forest."
+//                + " South there is a house. The door is open.");
+        this.defaultScenarioSceneList.add(newScene = new Garden());
+//        newScene = new Scene("You are in a room. East there is a kitchen."
+//                + " South you can see a staircase leading to the basement."
+//                + " North is an open door leading to a garden.");
+        this.defaultScenarioSceneList.add(newScene = new Room());
+//        newScene = new Scene("You are in a basement. North there is a staircase leading upwards.");
+        this.defaultScenarioSceneList.add(newScene = new Basement());
     }
 
     public void setScenarioComplete(Boolean scenarioComplete) {
