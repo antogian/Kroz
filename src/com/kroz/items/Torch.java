a7a016191c20c46b48cd89f01b75f486a9801204
Tony <Tony@Tony-PC.lan>
2015-06-01 22:09:08 +0300

    Added methods at <Torch.java> of items package

--- src/com/kroz/items/Torch.java
+++ src/com/kroz/items/Torch.java
@@ -1,7 +1,11 @@
 package com.kroz.items;
 
+import com.kroz.commands.DROP;
+import com.kroz.commands.LIGHT;
+import com.kroz.commands.TAKE;
 import com.kroz.enums.ItemType;
 import com.kroz.enums.ItemState;
+import com.kroz.scene.Scene;
 
 /**
 *
@@ -11,9 +15,10 @@ public class Torch extends Item {
 
     public Torch() {
         super("Torch", "an unused torch", ItemType.PLAYER_OBJECT, ItemState.DISABLED);
+        this.setLegalActions();
     }
-    public void useItem(){
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void useItem(Scene currentScene){
+        currentScene.setLighting(true);
     }
     /**
      * Implementation of the abstract method of the Item superclass.
@@ -26,5 +31,11 @@ public class Torch extends Item {
         else if (itemState == ItemState.ENABLED){
             itemState = ItemState.DISABLED;
         }
+    }
+    
+    public void setLegalActions(){
+        TAKE.addTakeableItem(this);
+        DROP.addDroppableItem(this);
+        LIGHT.addLightableItems(this);
     }
 }
